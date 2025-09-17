f65be68de304df7b862fd91b558e11733e059ddd
asJoshua <joshuaandrewsadleir@gmail.com>
2025-09-17 22:54:54 +0100

    added animations

--- src/components/about/AboutSection.tsx
+++ src/components/about/AboutSection.tsx
@@ -1,22 +1,29 @@
+import { motion } from "framer-motion"
 import { Container } from "../global/SectionContainer"
 import { TextContentWrapper } from "../global/TextContentWrapper"
+import { fadeInChild, fadeInParent } from "../../styles/animations"
 
 export const AboutSection = () => {
     return (
-        <Container flexDirection="column" style={{gap: '4rem'}}>
-            <TextContentWrapper>
-                <h2>ABOUT ME</h2>
-            </TextContentWrapper>
-            <iframe
-                src="https://www.youtube.com/embed/vAoB4VbhRzM?si=QESZQqooRDzpaoCh" // replace with intro YouTube link
-                title="About Me Video"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                style={{
-                width: "650px",
-                height: "500px",
-                }}
-            ></iframe>
+        <Container flexDirection="column">
+            <motion.div
+                variants={fadeInParent}
+                initial="hidden"
+                animate="show">
+                <TextContentWrapper >
+                    <motion.h2 variants={fadeInChild} style={{paddingBottom: '4rem'}}>ABOUT ME</motion.h2>
+                </TextContentWrapper>
+                <iframe
+                    src="https://www.youtube.com/embed/vAoB4VbhRzM?si=QESZQqooRDzpaoCh" // replace with intro YouTube link
+                    title="About Me Video"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    style={{
+                    width: "650px",
+                    height: "500px",
+                    }}
+                ></iframe>
+            </motion.div>
         </Container>
     )
 }
--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -1,15 +1,22 @@
+import { motion } from "framer-motion"
 import { Container } from "../global/SectionContainer"
 import { TextContentWrapper } from "../global/TextContentWrapper"
 import Carousel from "./Carousel"
+import { fadeInChild, fadeInParent } from "../../styles/animations"
 
 export const ProjectSection = () => {
     return (
         <Container flexDirection="column" style={{ gap:'0' }}>
-            <TextContentWrapper>
-                <h2>PROJECT SHOWCASE</h2>
-                <p>Last Updated - 20th of August</p>
-            </TextContentWrapper>
-            <Carousel></Carousel>
+            <motion.div
+            variants={fadeInParent}
+                    initial="hidden"
+                    animate="show">
+                <TextContentWrapper>
+                <motion.h2 variants={fadeInChild}>PROJECT SHOWCASE</motion.h2>
+                <motion.p variants={fadeInChild}>Last Updated - 20th of August</motion.p>
+                </TextContentWrapper>
+            </motion.div>
+                <Carousel></Carousel>
         </Container>
-    )
-}
+    );
+};
